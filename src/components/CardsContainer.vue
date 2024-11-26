44d3d61ff7ca09fd9e6fc3fb630a285fdd644835
Kujundzic-Minel <minel.kujundzic@edu.univ-fcomte.fr>
2024-11-26 22:09:44 +0100

    feat: :sparkles: add CardsContainer and ProgressBar components

---
+++ src/components/CardsContainer.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import HabitsCard from '@/components/HabitsCard.vue'
+import CardsCSS from './CardsCSS.vue'
+
+const habits = [
+  {
+    id: 1,
+    name: 'habit 1',
+    description: 'Méditation à la maison',
+    progressHabit: 70,
+  },
+  {
+    id: 2,
+    name: 'habit 2',
+    description: 'Méditation à la maison 2',
+    progressHabit: 10,
+  },
+  {
+    id: 3,
+    name: 'habit 3',
+    description: 'Méditation à la maison 3',
+    progressHabit: 50,
+  },
+]
+</script>
+
+<template>
+  <div>
+    <div v-for="habit in habits" :key="habit.id">
+      <HabitsCard :name="habit.name" :description="habit.description" />
+      <CardsCSS :progressHabit="habit.progressHabit" />
+    </div>
+  </div>
+</template>
---
+++ src/components/HabitsCard.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+defineProps({
+  name: String,
+  description: String,
+})
+</script>
+
+<template>
+  <div>
+    <h2>{{ name }}</h2>
+    <h3>{{ description }}</h3>
+  </div>
+</template>
