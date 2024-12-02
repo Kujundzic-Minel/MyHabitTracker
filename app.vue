7dda7fe390278f79ee3414316bb75a7665e96f81
MINEL <minel.kujundzic@edu.univ-fcomte.fr>
2024-12-02 13:21:16 +0100

    Feature(Page): Add Pages

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
   <div>
-    <CardsContainer />
+    <NuxtPage />
   </div>
 </template>
---
+++ pages/habits.vue
@@ -0,0 +1,5 @@
+<script setup lang="ts"></script>
+<template>
+  <div>Habits</div>
+  <CardsContainer />
+</template>
