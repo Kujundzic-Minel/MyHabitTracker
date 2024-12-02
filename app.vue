ee552497dd82166baad153601c04fcd0cd2c19f1
MINEL <minel.kujundzic@edu.univ-fcomte.fr>
2024-12-02 11:43:44 +0100

    setup(SCSS): fix files scss

--- app.vue
+++ app.vue
@@ -1,6 +1,5 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <CardsContainer />
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,13 +3,16 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   modules: ['@nuxt/eslint'],
+
+  css: ['@/assets/scss/main.scss'],
+
   vite: {
     css: {
       preprocessorOptions: {
         scss: {
           additionalData: `
-            @use "@/assets/scss/foundations/variables" as *;
-            @use "@/assets/scss/foundations/functions" as *;
+            @use "/assets/scss/foundations/variables" as *;
+            @use "/assets/scss/foundations/functions" as *;
           `,
         },
       },
