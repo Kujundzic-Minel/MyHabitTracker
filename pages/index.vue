aa11e35c34a7556126a665070e37d74fbccb8064
MINEL <minel.kujundzic@edu.univ-fcomte.fr>
2024-12-03 13:48:31 +0100

    feature(Sanity): Route parametrique "slug"

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,11 @@
 <script setup lang="ts">
 import type { SanityDocument } from '@sanity/client'
+import imageUrlBuilder from '@sanity/image-url'
+import type { SanityImageSource } from '@sanity/image-url/lib/types/types'
+const urlFor = (source: SanityImageSource) =>
+  projectId && dataset
+    ? imageUrlBuilder({ projectId, dataset }).image(source)
+    : null
 
 const POSTS_QUERY = groq`*[
   _type == "post"
@@ -7,6 +13,7 @@ const POSTS_QUERY = groq`*[
 ]|order(publishedAt desc)[0...12]{_id, title, slug, publishedAt, image}`
 
 const { data: posts } = await useSanityQuery<SanityDocument[]>(POSTS_QUERY)
+const { projectId, dataset } = useSanity().client.config()
 </script>
 
 <template>
@@ -14,10 +21,11 @@ const { data: posts } = await useSanityQuery<SanityDocument[]>(POSTS_QUERY)
     <h1 class="text-4xl font-bold mb-8">Posts</h1>
     <ul class="flex flex-col gap-y-4">
       <li v-for="post in posts" :key="post._id" class="hover:underline">
-        <nuxt-link :to="`/${post.slug.current}`">
+        <NuxtLink :to="`/${post.slug.current}`">
           <h2 class="text-xl font-semibold">{{ post.title }}</h2>
           <p>{{ new Date(post.publishedAt).toLocaleDateString() }}</p>
-        </nuxt-link>
+          <img v-if="post.image" :src="urlFor(post.image)?.width(200).height(200).url()">
+        </NuxtLink>
       </li>
     </ul>
   </main>
