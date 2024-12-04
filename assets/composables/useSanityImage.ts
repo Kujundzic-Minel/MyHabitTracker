88ccf780c30be7aba34d603eeca78676d46c8fb8
MINEL <minel.kujundzic@edu.univ-fcomte.fr>
2024-12-04 16:36:22 +0100

    feature(Sanity) : Composable Image for Sanity

--- assets/composables/useSanityImage.ts
+++ assets/composables/useSanityImage.ts
@@ -3,11 +3,9 @@ import type { SanityImageSource } from '@sanity/image-url/lib/types/types'
 
 export default function(){
   const { projectId, dataset } = useSanity().client.config()
-
   const urlFor = (source: SanityImageSource) =>
     projectId && dataset
       ? imageUrlBuilder({ projectId, dataset }).image(source)
       : null
-
   return { urlFor }
 }
