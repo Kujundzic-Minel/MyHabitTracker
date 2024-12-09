c507710934b72bfb801c7354eb2c1588d136d05a
MINEL <minel.kujundzic@edu.univ-fcomte.fr>
2024-12-09 10:52:28 +0100

    feature(Homepage) : Séparation de la hompage en plusieurs composants + ajout du header avec Sanity

--- components/Header.vue
+++ components/Header.vue
@@ -1,19 +1,49 @@
 <script setup lang="ts">
+interface Header {
+  logo: { asset: { url: string } };
+  navigation: { label: string; url: string }[];
+  cta: { label: string; url: string };
+}
+
+const { data: header } = await useSanityQuery<Header>(groq`
+*[_type == "header"][0]{
+  logo {
+    asset -> { url }
+  },
+  navigation[] {
+    label,
+    url
+  },
+  cta {
+    label,
+    url
+  },
+}
+`);
+
+console.log(header);
 </script>
+
 <template>
-  <header>
-    <nav>
-      <ul>
-        <li>
-          <nuxt-link to="/">Home</nuxt-link>
-        </li>
-        <li>
-          <nuxt-link to="/habits">Habits</nuxt-link>
-        </li>
-        <li>
-          <nuxt-link to="/about">About</nuxt-link>
+  <header v-if="header">
+    <!-- Logo -->
+    <div v-if="header.logo" class="header__logo">
+      <img :src="header.logo.asset.url" alt="Logo" />
+    </div>
+
+    <!-- Navigation -->
+    <nav class="header__nav">
+      <ul class="header__nav-list">
+        <li v-for="navItem in header.navigation" :key="navItem.label" class="header__nav-item">
+          <a :href="navItem.url" class="header__nav-link">{{ navItem.label }}</a>
         </li>
       </ul>
     </nav>
+
+    <!-- CTA -->
+    <div v-if="header.cta" class="header__cta">
+      <a :href="header.cta.url" class="cta-button">{{ header.cta.label }}</a>
+    </div>
   </header>
+
 </template>
