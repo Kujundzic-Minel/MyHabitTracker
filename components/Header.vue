c13a189e89c10def6982afd858e8744927879c0f
MINEL <minel.kujundzic@edu.univ-fcomte.fr>
2024-12-09 16:21:27 +0100

    feature(Colors) : Modifications des couleurs variables

--- components/Header.vue
+++ components/Header.vue
@@ -26,78 +26,20 @@ console.log(header);
 
 <template>
   <header v-if="header" class="header">
-    <div v-if="header.logo" class="header__logo">
-      <img :src="header.logo.asset.url" alt="Logo" class="header__logo-img" />
-    </div>
-
-    <nav class="header__nav">
-      <ul class="header__nav-list">
-        <li v-for="navItem in header.navigation" :key="navItem.label" class="header__nav-item">
-          <a :href="navItem.url" class="header__nav-link">{{ navItem.label }}</a>
-        </li>
-      </ul>
-    </nav>
-
-    <div v-if="header.cta" class="header__cta">
-      <a :href="header.cta.url" class="header__cta-button">{{ header.cta.label }}</a>
-    </div>
+    <aside>
+      <div v-if="header.logo" class="header__logo">
+        <img :src="header.logo.asset.url" alt="Logo" class="header__logo-img" />
+      </div>
+      <nav class="header__nav">
+        <ul class="header__nav-list">
+          <li v-for="navItem in header.navigation" :key="navItem.label" class="header__nav-item">
+            <a :href="navItem.url" class="header__nav-link">{{ navItem.label }}</a>
+          </li>
+        </ul>
+      </nav>
+      <div v-if="header.cta" class="header__cta">
+        <a :href="header.cta.url" class="header__cta-button">{{ header.cta.label }}</a>
+      </div>
+    </aside>
   </header>
 </template>
-
-<style lang="scss">
-.header {
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  padding: 1.5rem 2rem;
-  max-width: 100%;
-  margin: 0 auto;
-
-  &__logo {
-    flex-shrink: 0;
-
-    &-img {
-      height: 2.5rem;
-      width: auto;
-    }
-  }
-
-  &__nav {
-    &-list {
-      display: flex;
-      gap: 2rem;
-      list-style: none;
-      padding: 0;
-      margin: 0;
-    }
-
-    &-link {
-      text-decoration: none;
-      color: #333;
-      font-weight: 500;
-      transition: color 0.2s ease;
-
-      &:hover {
-        color: #666;
-      }
-    }
-  }
-
-  &__cta {
-    &-button {
-      display: inline-block;
-      padding: 0.75rem 1.5rem;
-      border-radius: 25px;
-      background-color: #333;
-      color: white;
-      text-decoration: none;
-      font-weight: 600;
-      transition: background-color 0.2s ease;
-
-      &:hover {
-        background-color: #555;
-      }
-    }
-  }
-}
-</style>
